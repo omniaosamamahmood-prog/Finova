bb46f7c06854c1b06f7ce0b390e260028d2b5047
Omnia Osama <omnia.osama.mahmood@gmail.com>
2026-08-03 14:25:57 +0300

    chore: initialize React project

---
+++ client/src/routes/AppRouter.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+function AppRouter(){
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+export default AppRouter;
